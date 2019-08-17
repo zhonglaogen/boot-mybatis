5f65ad022b990c8b3abf64351cc50aecd675cc23
zhonglaogen <ZLXzlxxs98@163.com>
2019-08-17 23:42:56 +0800

    add dubbo zk

--- consumer-user/src/main/java/com/example/user/service/UserService.java
+++ consumer-user/src/main/java/com/example/user/service/UserService.java
@@ -1,5 +1,17 @@
 package com.example.user.service;
 
-public class UserService {
+import com.example.ticket.service.TicketService;
 
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    //发布时按照接口犬类名发布的,根据全类名匹配到注册中心的服务
+    @Reference
+    TicketService ticketService;
+
+    public void hello(){
+        System.out.println("拿到的东西是"+ticketService.getTicket());
+    }
 }
--- ticket/src/main/java/com/example/ticket/service/TicketServiceImp.java
+++ ticket/src/main/java/com/example/ticket/service/TicketServiceImp.java
@@ -1,12 +1,13 @@
 package com.example.ticket.service;
 
 
+import org.apache.dubbo.config.annotation.Service;
 import org.springframework.stereotype.Component;
 
 
 @Component
 //将服务发布出去
-
+@Service
 public class TicketServiceImp implements TicketService {
     @Override
     public String getTicket() {
